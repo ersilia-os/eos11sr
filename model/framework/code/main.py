c964e34d37725c6abdb7c89b9de6dde1cefe31b2
Arnau <arnau@ersilia.io>
2025-07-07 16:11:44 +0200

    model incorporation

--- model/framework/code/main.py
+++ model/framework/code/main.py
@@ -2,8 +2,8 @@
 import os
 import csv
 import sys
+import numpy as np
 from rdkit import Chem
-from rdkit.Chem.Descriptors import MolWt
 
 # parse arguments
 input_file = sys.argv[1]
@@ -12,10 +12,8 @@ output_file = sys.argv[2]
 # current file directory
 root = os.path.dirname(os.path.abspath(__file__))
 
-# my model
-def my_model(smiles_list):
-    return [MolWt(Chem.MolFromSmiles(smi)) for smi in smiles_list]
-
+sys.path.append(root)
+from utils_molecules import convert_fp_to_embV2, calculate_morgan_fingerprints
 
 # read SMILES from .csv file, assuming one column with header
 with open(input_file, "r") as f:
@@ -24,7 +22,10 @@ with open(input_file, "r") as f:
     smiles_list = [r[0] for r in reader]
 
 # run model
-outputs = my_model(smiles_list)
+molecules = [Chem.MolFromSmiles(mol) for mol in smiles_list]
+morgan_fps = calculate_morgan_fingerprints(molecules, radius=2, nbits=16384)
+emfps = convert_fp_to_embV2(morgan_fps, size=16)
+outputs = emfps
 
 #check input and output have the same lenght
 input_len = len(smiles_list)
@@ -34,6 +35,7 @@ assert input_len == output_len
 # write output in a .csv file
 with open(output_file, "w") as f:
     writer = csv.writer(f)
-    writer.writerow(["value"])  # header
+    header = ["dim_{0}".format(str(i).zfill(4)) for i in range(1024)]
+    writer.writerow(header)  # header
     for o in outputs:
-        writer.writerow([o])
+        writer.writerow(o)
