188d6e70b51b7e45e30ad8b6fb5b4db6d2cb23f9
Arnau <arnau@ersilia.io>
2025-07-17 14:59:34 +0200

    setting 256 output dimensions

--- model/framework/code/main.py
+++ model/framework/code/main.py
@@ -24,7 +24,7 @@ with open(input_file, "r") as f:
 # run model
 molecules = [Chem.MolFromSmiles(mol) for mol in smiles_list]
 morgan_fps = calculate_morgan_fingerprints(molecules, radius=2, nbits=16384)
-emfps = convert_fp_to_embV2(morgan_fps, size=16)
+emfps = convert_fp_to_embV2(morgan_fps, size=64)
 outputs = emfps
 
 #check input and output have the same lenght
@@ -35,7 +35,7 @@ assert input_len == output_len
 # write output in a .csv file
 with open(output_file, "w") as f:
     writer = csv.writer(f)
-    header = ["dim_{0}".format(str(i).zfill(4)) for i in range(1024)]
+    header = ["dim_{0}".format(str(i).zfill(3)) for i in range(256)]
     writer.writerow(header)  # header
     for o in outputs:
         writer.writerow(o)
