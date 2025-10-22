16037ea90a4e72ba7a8df1125095127eb5d73e77
Marina18 <marina@ersilia.io>
2025-10-22 11:26:53 +0200

    ersilia_pack_utils refactor

--- model/framework/code/main.py
+++ model/framework/code/main.py
@@ -4,7 +4,7 @@ import csv
 import sys
 import numpy as np
 from rdkit import Chem
-
+from ersilia_pack_utils.core import write_out, read_smiles
 # parse arguments
 input_file = sys.argv[1]
 output_file = sys.argv[2]
@@ -16,11 +16,9 @@ sys.path.append(root)
 from utils_molecules import convert_fp_to_embV2, calculate_morgan_fingerprints
 
 # read SMILES from .csv file, assuming one column with header
-with open(input_file, "r") as f:
-    reader = csv.reader(f)
-    next(reader)  # skip header
-    smiles_list = [r[0] for r in reader]
+cols, smiles_list = read_smiles(input_file)
 
+print(smiles_list)
 # run model
 molecules = [Chem.MolFromSmiles(mol) for mol in smiles_list]
 morgan_fps = calculate_morgan_fingerprints(molecules, radius=2, nbits=16384)
@@ -31,11 +29,15 @@ outputs = emfps
 input_len = len(smiles_list)
 output_len = len(outputs)
 assert input_len == output_len
+print(outputs)
 
+header = ["dim_{0}".format(str(i).zfill(3)) for i in range(256)]
+
+write_out(outputs,header,output_file,dtype='float32')
 # write output in a .csv file
-with open(output_file, "w") as f:
-    writer = csv.writer(f)
-    header = ["dim_{0}".format(str(i).zfill(3)) for i in range(256)]
-    writer.writerow(header)  # header
-    for o in outputs:
-        writer.writerow(o)
+# with open(output_file, "w") as f:
+#     writer = csv.writer(f)
+#     header = ["dim_{0}".format(str(i).zfill(3)) for i in range(256)]
+#     writer.writerow(header)  # header
+#     for o in outputs:
+#         writer.writerow(o)
