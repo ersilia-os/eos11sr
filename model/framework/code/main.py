40b927e302447d42d191057587e14122de1de4d5
Arnau <arnau@ersilia.io>
2025-10-23 11:43:15 +0200

    setting dims to 32

--- model/framework/code/main.py
+++ model/framework/code/main.py
@@ -4,6 +4,7 @@ import csv
 import sys
 import numpy as np
 from rdkit import Chem
+from collections import Counter
 from ersilia_pack_utils.core import write_out, read_smiles
 # parse arguments
 input_file = sys.argv[1]
@@ -18,26 +19,20 @@ from utils_molecules import convert_fp_to_embV2, calculate_morgan_fingerprints
 # read SMILES from .csv file, assuming one column with header
 cols, smiles_list = read_smiles(input_file)
 
-print(smiles_list)
 # run model
 molecules = [Chem.MolFromSmiles(mol) for mol in smiles_list]
-morgan_fps = calculate_morgan_fingerprints(molecules, radius=2, nbits=16384)
-emfps = convert_fp_to_embV2(morgan_fps, size=64)
+morgan_fps = calculate_morgan_fingerprints(molecules, radius=2, nbits=16384).astype(int)
+emfps = convert_fp_to_embV2(morgan_fps, size=512)
 outputs = emfps
 
 #check input and output have the same lenght
 input_len = len(smiles_list)
 output_len = len(outputs)
 assert input_len == output_len
-print(outputs)
 
-header = ["dim_{0}".format(str(i).zfill(3)) for i in range(256)]
+# for i, j in zip(outputs, morgan_fps):
+# 	print(Counter(j)[0], len(j), Counter(i)[0], len(i))
 
+# write output
+header = ["dim_{0}".format(str(i).zfill(2)) for i in range(32)]
 write_out(outputs,header,output_file,dtype='float32')
-# write output in a .csv file
-# with open(output_file, "w") as f:
-#     writer = csv.writer(f)
-#     header = ["dim_{0}".format(str(i).zfill(3)) for i in range(256)]
-#     writer.writerow(header)  # header
-#     for o in outputs:
-#         writer.writerow(o)
