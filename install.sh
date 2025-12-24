6bc7ea5eaa47ea144e34dd34ee403526a856d685
gemmaturon <gemma@ersilia.io>
2025-12-24 11:12:36 +0100

    Delete install.sh [skip ci]

--- install.sh
+++
@@ -1,8 +0,0 @@
-/home/marina/anaconda3/envs/eos11sr/bin/python -m pip install pandas==2.3.0
-/home/marina/anaconda3/envs/eos11sr/bin/python -m pip install numpy==2.2.6
-/home/marina/anaconda3/envs/eos11sr/bin/python -m pip install matplotlib==3.10.3
-/home/marina/anaconda3/envs/eos11sr/bin/python -m pip install rdkit==2025.3.3
-/home/marina/anaconda3/envs/eos11sr/bin/python -m pip install tqdm==4.67.1
-/home/marina/anaconda3/envs/eos11sr/bin/python -m pip install psutil==7.0.0
-/home/marina/anaconda3/envs/eos11sr/bin/python -m pip install scikit-learn==1.7.0
-/home/marina/anaconda3/envs/eos11sr/bin/python -m pip install ersilia-pack-utils==0.1.2
